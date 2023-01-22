fac71cf5f371bd7103e3069d6f7efd667025ea57
Michel <miche1ss433@gmail.com>
2023-01-22 19:53:28 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Horas</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="static/style.css">
+  
+</head>
+<body style="background-color: white;">
+
+    <main class="form-id-main text-center">
+        <div class="form-id">
+            <form method="post">
+                <h1 class="h3 mb-3 fw-normal">Insira seu ID:</h1>
+            
+                <div class="form-floating mb-4">
+                    <input name="id_user" type="number" class="form-control" id="floatingPassword" placeholder="ID..." required>
+                    <label for="floatingPassword">ID . . .</label>
+                </div>
+            
+                <button class="w-100 btn btn-lg btn-secondary" name="btn_id" type="submit">Entrar</button>
+                <p class="mt-5 mb-3 text-muted">© Michel Soares</p>
+            </form>
+        </div>
+    <main>         
+
+</body>
+</html>
